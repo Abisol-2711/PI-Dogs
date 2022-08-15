1ef2fd5f48b2322882769db8f0c34ca9c24e902c
Sol <solabigail00@hotmail.com>
2022-08-15 00:33:44 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js'); 
 const { conn } = require('./src/db.js');
 
+const port = process.env.PORT || 3001;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen((process.env.PORT || 3001), () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001');
   });
 });
